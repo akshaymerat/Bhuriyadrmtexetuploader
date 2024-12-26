5d2f5d68c408782fb0b3baada5e5f3666edc1b5a
akshaymerat <akshaymerat@gmail.com>
2024-12-26 13:15:46 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,5 +1,5 @@
 api_id = 24812063
 api_hash = "e7ee18967768d95aeb7f2f6674e7da4c"
-bot_token = "7806242713:AAFLLUD3Kp011YSn4-R8CIPy0jREKcKzHCI"
+bot_token = ""
 AUTH = 6562642609
 #Bot Username = "bhuriyabhaig"
